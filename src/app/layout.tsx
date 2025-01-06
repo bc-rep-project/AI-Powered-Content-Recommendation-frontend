f5fb50065fb504322f4d503a0c03bae6015d86be
bc-rep-project <johannderbiel@gmail.com>
2025-01-06 11:33:05 +0200

    Update import path for Layout component in layout.tsx to use alias for improved code organization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Layout from '../components/layout';
+import Layout from '@/components/layout';
 import { ThemeProvider } from '@/providers/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] });
